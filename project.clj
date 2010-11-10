48a7e1d46f6072a0b7328eeedc3cc2a1aef37a22
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-11-10 15:48:50 +0900

    add lein-clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[ring/ring-core "0.3.3"]
+		     [lein-clojars "0.6.0"]
 		     [swank-clojure "1.2.1"]])
