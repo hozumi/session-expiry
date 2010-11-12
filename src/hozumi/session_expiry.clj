e8f81dcf4aa955a6cf037e268b05a759843490d5
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-11-12 14:03:46 +0900

    pre compute exipired-ms

--- src/hozumi/session_expiry.clj
+++ src/hozumi/session_expiry.clj
@@ -1,20 +1,21 @@
 (ns hozumi.session-expiry
   (:import [java.util Date]))
 
-(defn- expire? [date expire-sec]
-  (< (* 1000 expire-sec)
+(defn- expire? [date expire-ms]
+  (< expire-ms
      (- (.getTime (Date.)) (.getTime date))))
 
 (defn wrap-session-expiry [handler expire-sec]
-  (fn [{{date ::date :as req-session} :session :as request}]
-    (let [request  (if (and date (expire? date expire-sec))
-		     (assoc request :session {})
-		     request)
-	  response (handler request)]
-      (if (contains? response :session)
-	(if (response :session)
-	  (assoc-in response [:session ::date] (Date.))
-	  response)
-	(if (empty? req-session)
-	  response
-	  (assoc response :session (assoc req-session ::date (Date.))))))))
+  (let [expire-ms (* 1000 expire-sec)]
+    (fn [{{date ::date :as req-session} :session :as request}]
+      (let [request  (if (and date (expire? date expire-ms))
+		       (assoc request :session {})
+		       request)
+	    response (handler request)]
+	(if (contains? response :session)
+	  (if (response :session)
+	    (assoc-in response [:session ::date] (Date.))
+	    response)
+	  (if (empty? req-session)
+	    response
+	    (assoc response :session (assoc req-session ::date (Date.)))))))))
