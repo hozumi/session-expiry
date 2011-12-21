7a7e837da003c7585f8fb456c6d0160b6af9364f
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2011-12-21 17:36:19 +0900

    update dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
 (defproject org.clojars.hozumi/session-expiry "1.0.0-SNAPSHOT"
   :description "A ring middleware for setting session timeout."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[ring/ring-core "0.3.3"]
-		     [lein-clojars "0.6.0"]
-		     [swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "[1.2.0,)"]]
+  :dev-dependencies [[ring/ring-core "[0.3.3,)"]])
