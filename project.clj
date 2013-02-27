1af076c39916838139dfe9f342d61e78f1413b97
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2013-02-28 00:56:11 +0900

    remove range dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject org.clojars.hozumi/session-expiry "1.0.0-SNAPSHOT"
   :description "A ring middleware for setting session timeout."
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]]
-  :dev-dependencies [[ring/ring-core "[0.3.3,)"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dev-dependencies [[ring/ring-core "0.3.3"]])
